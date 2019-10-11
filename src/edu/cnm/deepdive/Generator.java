1af5b20933e48e1c21fecf3b6a97f609e5f40b50
Brian Bleck <bbleck@unm.edu>
2019-10-11 09:17:23 -0600

    Initial solution

--- src/edu/cnm/deepdive/Generator.java
+++ src/edu/cnm/deepdive/Generator.java
@@ -8,26 +8,32 @@ package edu.cnm.deepdive;
  */
 public class Generator {
 
-  private Generator() {
-    // NOTE: There is NO need to do anything with this constructor! The methods
-    //       you will implement in this class are static; thus, there is no need
-    //       to create instances of this class.
-  }
+    private Generator() {
+        // NOTE: There is NO need to do anything with this constructor! The methods
+        //       you will implement in this class are static; thus, there is no need
+        //       to create instances of this class.
+    }
 
-  /* BASIC PROBLEM */
+    /* BASIC PROBLEM */
 
-  /**
-   * Creates and returns an <code>int[]</code> containing a sequence of alternating positive and
-   * negative squared values, of the form (-1)<sup>n</sup>n<sup>2</sup>, where n =
-   * 0&hellip;(<code>length</code> - 1).
-   * <p>For example, <code>Generator.oscillatingSquares(5)</code> returns an array containing the
-   * values <code>{0, -1, 4, -9, 16}</code>.</p>
-   *
-   * @param length number of elements in array.
-   * @return generated array of oscillating squares.
-   */
-  public static int[] oscillatingSquares(int length) {
-    return null; // TODO Replace this line with implementation.
-  }
+    /**
+     * Creates and returns an <code>int[]</code> containing a sequence of alternating positive and
+     * negative squared values, of the form (-1)<sup>n</sup>n<sup>2</sup>, where n =
+     * 0&hellip;(<code>length</code> - 1).
+     * <p>For example, <code>Generator.oscillatingSquares(5)</code> returns an array containing the
+     * values <code>{0, -1, 4, -9, 16}</code>.</p>
+     *
+     * @param length number of elements in array.
+     * @return generated array of oscillating squares.
+     */
+    public static int[] oscillatingSquares(int length) {
+        int[] toReturn = new int[length];
+        int negator = -1;
+        for (int i = 0; i < length; i++) {
+            negator *= (-1);
+            toReturn[i] = (negator * i * i);
+        }
+        return toReturn;
+    }
 
 }
