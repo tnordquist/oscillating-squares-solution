2b75b82e95ba0ec0641b25546db5f9acad7fe097
Todd A Nordquist <toddnordquist144@gmail.com>
2019-10-11 09:25:35 -0600

    Added test file

---
+++ test/edu/cnm/deepdive/GeneratorTest.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeneratorTest {
+
+  @Test
+  void oscillatingSquares() {
+  }
+}
