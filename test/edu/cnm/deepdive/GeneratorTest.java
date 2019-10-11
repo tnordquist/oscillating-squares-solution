e68a6c19f5e7aa993f812beed5bd6a041fd50114
Brian Bleck <bbleck@unm.edu>
2019-10-11 09:36:00 -0600

    Added test.

--- test/edu/cnm/deepdive/GeneratorTest.java
+++ test/edu/cnm/deepdive/GeneratorTest.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class GeneratorTest {
 
+  int[] testArr = {0, -1, 4, -9, 16, -25, 36, -49, 64, -81, 100, -121, 144, -169};
+
   @Test
   void oscillatingSquares() {
+    for (int i = 0; i < testArr.length; i++) {
+      assertEquals(testArr[i], Generator.oscillatingSquares(testArr.length)[i]);
+    }
   }
 }
